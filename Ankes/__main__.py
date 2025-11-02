519950d797fe13bbdf05c514928ab20c7465fcfa
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 16:04:00 +0700

    Update __main__.py

--- Ankes/__main__.py
+++ Ankes/__main__.py
@@ -1,1 +1,18 @@
 import asyncio
+import importlib
+
+from pyrogram import idle
+from config import LOGGER
+from Ankes import app
+
+
+async def main():
+    await app.start()
+    await idle()
+    await app.stop()
+    LOGGER("JamalasxAnkes").info("ankes dinonaktifkan")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+
