658df46e9e9c54dcc7ee80bdde6e17fd4b2c8aea
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 16:20:31 +0700

    Create start.py

---
+++ Ankes/modules/start.py
@@ -0,0 +1,8 @@
+from pyrogram import filters
+from pyrogram.enums import ChatType
+
+from Ankes import app
+
+
+@app.on_message(filters.command(["start"]) & filters.private)
+async def start_pm(client, message: Message, _):
