b1c94ad4d36d13a69377df69e5c788aa2ac5e587
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 15:53:27 +0700

    Update __init__.py

--- Ankes/__init__.py
+++ Ankes/__init__.py
@@ -14,8 +14,20 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+            max_concurrent_transmission=7,
         )
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
 
+        LOGGER(__name__).info(f"ankes [ {self.name} ] diaktifkan")
 
-
+    async def stop(self):
+        await super().stop()
+        LOGGER(__name__).info("bot dinonaktifkan")
 
