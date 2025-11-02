253c412e35dbc9d0cd46c0d75f68a4fd4c50a192
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 15:43:16 +0700

    Update __init__.py

--- Ankes/__init__.py
+++ Ankes/__init__.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters
 
 from config import (
-API_ID,
-API_HASH,
-BOT_TOKEN,
-LOGGER,
+    API_ID,
+    API_HASH,
+    BOT_TOKEN,
+    LOGGER,
 )
 
 class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="ankes",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+        )
 
 
 
