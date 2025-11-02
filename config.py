75aac602ecd281f8865afe7b776bf3fe2270e267
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 16:08:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 API_ID = int(os.environ.get("API_ID", "21839531"))
-API_HASH = os.environ.get("API_HASH", "2ea72246a26c4c29cc419175f738efa3"))
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7946571542:AAG8YjxNGjh0blV95VcTTjt5Hmc5ved-pfs"))
+API_HASH = os.environ.get("API_HASH", "2ea72246a26c4c29cc419175f738efa3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7946571542:AAG8YjxNGjh0blV95VcTTjt5Hmc5ved-pfs")
 
 
 LOG_FILE_NAME = "logs.txt"
