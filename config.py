9450df49d44d0e08b1159fff5e939c6e901813bb
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 15:38:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,18 @@ API_HASH = os.environ.get("API_HASH", ""))
 BOT_TOKEN = os.environ.get("BOT_TOKEN", ""))
 
 
+LOG_FILE_NAME = "logs.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
