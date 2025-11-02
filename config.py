e268b3bb1fa5d08fb268a382113e49303e0b895b
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 15:35:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import logging
 import os
+
+from distutils.util import strtobool
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+
+load_dotenv("config.env")
+
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", ""))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", ""))
+
+
