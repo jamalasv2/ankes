b032ca05a382e3fab93d489094105aa4072f8470
jamalasv2 <jamalasbanget@gmail.com>
2025-11-02 16:23:40 +0700

    Update __main__.py

--- Ankes/__main__.py
+++ Ankes/__main__.py
@@ -2,12 +2,17 @@ import asyncio
 import importlib
 
 from pyrogram import idle
+
 from config import LOGGER
 from Ankes import app
+from Ankes.modules import ALL_MODULES
 
 
 async def main():
     await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Ankes.modules" + all_module)
+        LOGGER("JamalasxPlugins").info("berhasil memuat semua modul")
     await idle()
     await app.stop()
     LOGGER("JamalasxAnkes").info("bot ankes dinonaktifkan")
